597c0d28b9042b65808780e12c5003b18f9f28c5
Dmitry <73519131+ZorinDmitri@users.noreply.github.com>
2024-09-05 13:05:15 +0200

    Create aWSFiles.js

---
+++ aWSFiles.js
@@ -0,0 +1,272 @@
+import { LightningElement, track, wire } from "lwc";
+import awssdk from "@salesforce/resourceUrl/AWSSDK";
+import { loadScript } from "lightning/platformResourceLoader";
+
+import getS3Creds from "@salesforce/apex/AWSCredentialSelector.getS3Creds";
+
+import fetchContentVersions from "@salesforce/apex/AWSFileController.fetchContentVersions";
+import SIZE from "@salesforce/schema/ContentVersion.ContentSize";
+import MODIFIED_DATE from "@salesforce/schema/ContentVersion.ContentModifiedDate";
+import PATH from "@salesforce/schema/ContentVersion.PathOnClient";
+
+import { NavigationMixin } from "lightning/navigation";
+
+const BUTTON_LABEL = "Preview";
+const BUCKET = "unity-oena";
+
+export default class AWSMockup extends NavigationMixin(LightningElement) {
+  columns = [
+    {
+      label: "File name",
+      fieldName: PATH.fieldApiName,
+      sortable: false,
+      editable: false,
+      initialWidth: 360
+    },
+    {
+      label: "Size, bytes",
+      fieldName: SIZE.fieldApiName,
+      sortable: false,
+      editable: false,
+      initialWidth: 200
+    },
+    {
+      label: "Last Modified Date",
+      fieldName: MODIFIED_DATE.fieldApiName,
+      sortable: false,
+      editable: false,
+      initialWidth: 240
+    },
+    {
+      type: "button",
+      initialWidth: 120,
+      typeAttributes: {
+        label: BUTTON_LABEL,
+        name: BUTTON_LABEL,
+        title: BUTTON_LABEL,
+        disabled: false,
+        value: BUTTON_LABEL,
+        variant: "Brand"
+      }
+    }
+  ];
+
+  keyId;
+  keySecret;
+  @track fileName;
+  @track showSpinner = false;
+  fileToUpload;
+  wiredMockData;
+  error;
+
+  @wire(fetchContentVersions)
+  wiredPlans(result) {
+    if (result.data) {
+      this.wiredMockData = result.data;
+      this.error = undefined;
+    } else if (result.error) {
+      this.error = result.error;
+      this.wiredMockData = undefined;
+      console.log("Error: " + this.error);
+    }
+  }
+
+  renderedCallback() {
+    Promise.all([loadScript(this, awssdk)])
+      .then(() => {
+        this.getObjectsFromS3();
+      })
+      .catch((error) => {
+        console.error("error -> " + error);
+      });
+  }
+
+  async getObjectsFromS3() {
+    console.log("retrieving file list..");
+
+    const AWS = window.AWS;
+
+    getS3Creds().then((result) => {
+      this.keyId = result.AWSS3AccessKeyId__c;
+      this.keySecret = result.AWSS3AccessKeySecret__c;
+
+      AWS.config.update({
+        apiVersion: "2006-03-01",
+        accessKeyId: this.keyId,
+        secretAccessKey: this.keySecret,
+        region: "eu-central-1",
+        bucket: BUCKET
+      });
+
+      var s3 = new AWS.S3();
+
+      async function getSignedUrl() {
+        return new Promise((resolve, reject) => {
+          let params = {
+            Bucket: BUCKET,
+            MaxKeys: 1000
+          };
+          s3.getSignedUrl("listObjectsV2", params, (err, url) => {
+            if (err) {
+              console.log("S3 error: " + err);
+              reject(err);
+            }
+            resolve(url);
+          });
+        });
+      }
+
+      async function processFileList() {
+        const signedUrl = await getSignedUrl();
+        console.log("fileList URL: " + signedUrl);
+        fetch(signedUrl, {
+          method: "Get"
+        })
+          .then((response) => {
+            return response;
+          })
+          .catch((error) => {
+            console.log("file listing error caught");
+            console.error(error);
+          });
+      }
+
+      processFileList().then((res) => {
+        var parser = new DOMParser();
+        var xml = parser.parseFromString(res, "application/xml");
+        const jsonData = JSON.stringify(xml);
+        console.dirxml("xml: " + xml);
+        console.log("jsonData: " + jsonData);
+      });
+    });
+  }
+
+  handleSelectedFiles(event) {
+    if (event.target.files.length > 0) {
+      this.fileToUpload = event.target.files[0];
+      this.fileName = event.target.files[0].name;
+      console.log("fileName ====> " + this.fileName);
+      this.uploadToAWS(this.fileName, this.fileToUpload);
+    }
+  }
+
+  async uploadToAWS(fileName, fileBody) {
+    console.log("uploadToAWS...");
+
+    const AWS = window.AWS;
+
+    getS3Creds().then((result) => {
+      this.keyId = result.AWSS3AccessKeyId__c;
+      this.keySecret = result.AWSS3AccessKeySecret__c;
+
+      AWS.config.update({
+        apiVersion: "2006-03-01",
+        accessKeyId: this.keyId,
+        secretAccessKey: this.keySecret,
+        region: "eu-central-1",
+        bucket: BUCKET
+      });
+
+      var s3 = new AWS.S3();
+
+      async function getSignedUrl(fileName) {
+        return new Promise((resolve, reject) => {
+          let params = {
+            Bucket: BUCKET,
+            ContentType: fileBody.type,
+            Key: fileName
+          };
+          s3.getSignedUrl("putObject", params, (err, url) => {
+            if (err) {
+              console.log("S3 error: " + err);
+              reject(err);
+            }
+            resolve(url);
+          });
+        });
+      }
+
+      async function uploadFile(fileName) {
+        const signedUrl = await getSignedUrl(fileName);
+        console.log("upload file URL: " + signedUrl);
+
+        fetch(signedUrl, {
+          method: "PUT",
+          body: fileBody
+        })
+          .then((response) => {
+            return response;
+          })
+          .catch((error) => {
+            console.log("file upload error caught");
+            console.error(error);
+          });
+      }
+
+      uploadFile(fileName).then((res) => {
+        console.log(res);
+        console.log("uploadToAWS Finish...");
+      });
+    });
+  }
+
+  async showDocumentLink() {
+    const AWS = window.AWS;
+
+    getS3Creds().then((result) => {
+      this.keyId = result.AWSS3AccessKeyId__c;
+      this.keySecret = result.AWSS3AccessKeySecret__c;
+
+      AWS.config.update({
+        accessKeyId: this.keyId,
+        secretAccessKey: this.keySecret,
+        region: "eu-central-1",
+        bucket: "unity-oena"
+      });
+
+      var s3 = new AWS.S3();
+
+      var strImageUrl =
+        "https://unity-oena.s3.eu-central-1.amazonaws.com/Partnerspiel_Stammbaum_1";
+      if (strImageUrl != null && strImageUrl != "") {
+        var keyindex = strImageUrl.lastIndexOf("/");
+        var strKey = strImageUrl.substring(keyindex + 1);
+      }
+
+      async function getSignedUrl() {
+        return new Promise((resolve, reject) => {
+          let params = {
+            Bucket: BUCKET,
+            Key: strKey
+          };
+          s3.getSignedUrl("getObject", params, (err, url) => {
+            if (err) {
+              console.log("S3 error: " + err);
+              reject(err);
+            }
+            resolve(url);
+          });
+        });
+      }
+
+      async function processFileURL() {
+        const signedUrl = await getSignedUrl();
+        return signedUrl;
+      }
+
+      processFileURL().then((result) => {
+        this.pdfUrl = result;
+        this[NavigationMixin.Navigate]({
+          type: "standard__webPage",
+          attributes: {
+            url: this.pdfUrl
+          }
+        });
+      });
+    });
+  }
+
+  renderFile() {
+    this.showDocumentLink();
+  }
+}
